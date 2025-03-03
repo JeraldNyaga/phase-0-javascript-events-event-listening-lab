24b5ff20d72b00e044ce086444ac5ffa4b0bd964
judgeOstrich <jeraldnyaga15@gmail.com>
2025-03-03 03:04:50 +0300

    Add a click event listener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById("button");
+    input.addEventListener("click", clickAlert);
+}
+
+function clickAlert(){
+    alert("I was clicked");
 }
